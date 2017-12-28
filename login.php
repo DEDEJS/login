b8e0b5b1436850b9d2e83170ac617b56c49ac2fd
DEDEJS <andreaparecido892@gmail.com>
2017-12-28 05:43:43 -0200

    login.php

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+ <head>
+  <meta charset="utf-8">
+  <title>Login</title>	 
+ </head>
+<body>
+ <h3>Cadastro</h3>
+<input type="text" name="nome">
+<input type="email" name="email">
+</body>
+</html>
